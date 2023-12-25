268dcf07a82decbb07ea9e3054923fffb8ca4011
Eduardo Lima <natos.10@hotmail.com>
2023-12-25 08:02:25 -0300

    update dist

--- build/lib/duds_converters/utils.py
+++ build/lib/duds_converters/utils.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import List
 
 
 def get_existent_file_path(file_path: str | Path) -> Path:
@@ -24,3 +25,47 @@ def is_img(file):
             return True
 
     return False
+
+
+
+def split_file_path(path: str | Path) -> List[str]:
+    """
+    Splits a file path into its directory path, file name, and extension.
+
+    Args:
+        path (str | Path): The path of the file.
+
+    Returns:
+        List[str]: A list containing the directory path, file name, and extension.
+    """
+    file_path = get_existent_file_path(path)
+    directory, file_name = file_path.parent, file_path.stem
+    extension = file_path.suffix
+    return [str(directory), str(file_name), extension]
+
+
+def build_file_path(directory: str | Path, file_name: str, extension: str) -> Path:
+    """
+    Builds a file path from its directory path, file name, and extension.
+
+    Args:
+        directory (str | Path): The directory path.
+        file_name (str): The name of the file.
+        extension (str): The file extension.
+
+    Returns:
+        str: The complete file path.
+    """
+    # Convert directory to Path if it's given as a string
+    directory_path = Path(directory) if isinstance(directory, str) else directory
+
+    # Join the directory, file name, and extension to form the complete file path
+    file_path = directory_path / f"{file_name}{extension}"
+
+    return file_path
+
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read()
-
 setup(
     name="conversores_dudu",
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        requirements
+        "moviepy>=1.0.3",
+        "numpy>=1.26.2",
+        "Pillow>=10.1.0",
+        "twine>=4.0.2",
+        "types-Pillow>=10.1.0.2",
     ],
 )
