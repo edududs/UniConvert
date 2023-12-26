494224354e7fe8b0393281f143840680ee2b6451
Eduardo Lima <natos.10@hotmail.com>
2023-12-26 11:47:01 -0300

    change version on setup for 0.1.1 and build new dist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="UniConvert",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "certifi==2023.11.17",
