0bb5ffdfad3f27448e400d32291857d6676d91da
Eduardo Lima <natos.10@hotmail.com>
2023-12-26 14:20:51 -0300

    fix import problems

--- uniconvert/converters.py
+++ uniconvert/converters.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from base_converters import BaseIMGConverter, BaseVideoConverter
+from uniconvert.base_converters import BaseIMGConverter, BaseVideoConverter
 
 
 class PNGConverter(BaseIMGConverter):
